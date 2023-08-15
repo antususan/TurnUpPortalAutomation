d4a1ab8404a1b1e0740e6782a9b2a2cd20655b33
Antu Susan <antususansunila@gmail.com>
2023-08-15 16:17:07 +1200

    Adding the inital codebase to github

---
+++ TurnUpPortalAutomation/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//open the browser 
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+// Launch turnup portal and navigation to login
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+
+//Identify username textbox and enter Valid username 
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+//Identify password textbox and enter valid password 
+IWebElement password = driver.FindElement(By.Id("Password"));
+password.SendKeys("123123");
+
+//Identify login button and clck on the button 
+IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginbutton.Click();
+
+//check if the user has logged in sucessfully 
+IWebElement Hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if (Hellohari.Text == "Hello hari!")
+{
+    Console.WriteLine("user logged in sucessfully");
+
+}
+else
+{
+    Console.WriteLine("user hasn't been logged in ");
+}
+
+
+
